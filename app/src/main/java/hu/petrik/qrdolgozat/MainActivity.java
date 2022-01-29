f1e98d40d1848d2f62d9c69da2d47b876c53f2db
Szalai Szabolcs <sz.szalai16@gmail.com>
2022-01-29 17:00:12 +0100

    scan működik

--- app/src/main/java/hu/petrik/qrdolgozat/MainActivity.java
+++ app/src/main/java/hu/petrik/qrdolgozat/MainActivity.java
@@ -1,14 +1,68 @@
 package hu.petrik.qrdolgozat;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnScan, btnKiir;
+    private TextView textViewSzoveg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+        btnScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator intentIntegrator = new IntentIntegrator(MainActivity.this);
+                intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+                intentIntegrator.setPrompt("QR Code Scanner");
+                intentIntegrator.setBeepEnabled(false);
+                intentIntegrator.initiateScan();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            String seged = textViewSzoveg.getText().toString();
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Kiléptél a scannelésből", Toast.LENGTH_SHORT).show();
+            } else {
+                textViewSzoveg.setText(result.getContents());
+                try {
+                    Uri uri = Uri.parse(seged);
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                    startActivity(intent);
+                } catch (Exception e) {
+                    Log.d("URI ERROR", e.toString());
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void init() {
+        btnScan = findViewById(R.id.btnScan);
+        btnKiir = findViewById(R.id.btnKiir);
+        textViewSzoveg = findViewById(R.id.textViewSzoveg);
     }
 }
