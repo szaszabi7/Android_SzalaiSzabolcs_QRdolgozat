50e4ec04ab6ff2617bf76524c16436d0cd15ba14
Szalai Szabolcs <sz.szalai16@gmail.com>
2022-01-29 18:15:46 +0100

    storage permission

--- app/src/main/java/hu/petrik/qrdolgozat/MainActivity.java
+++ app/src/main/java/hu/petrik/qrdolgozat/MainActivity.java
@@ -1,9 +1,13 @@
 package hu.petrik.qrdolgozat;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,10 +19,17 @@ import android.widget.Toast;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button btnScan, btnKiir;
     private TextView textViewSzoveg;
+    private boolean writePermission;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +46,27 @@ public class MainActivity extends AppCompatActivity {
                 intentIntegrator.setPrompt("QR Code Scanner");
                 intentIntegrator.setBeepEnabled(false);
                 intentIntegrator.initiateScan();
+            }
+        });
+
+        btnKiir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (writePermission) {
+                    Date date = Calendar.getInstance().getTime();
+                    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    String s = textViewSzoveg.getText().toString() + ", " + format.format(date);
+
+                    try {
+                        FileOutputStream fileOutputStream = openFileOutput("scannedCodes.csv", MODE_PRIVATE);
+                        fileOutputStream.write(s.getBytes());
+
+                        Toast.makeText(MainActivity.this, "Sikeresen elmentve: " + getFilesDir() + "/scannedCodes.csv", Toast.LENGTH_SHORT).show();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
             }
         });
     }
@@ -60,9 +92,35 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 0) {
+            writePermission =
+                    ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                            == PackageManager.PERMISSION_GRANTED &&
+                            ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                                    == PackageManager.PERMISSION_GRANTED;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
     public void init() {
         btnScan = findViewById(R.id.btnScan);
         btnKiir = findViewById(R.id.btnKiir);
         textViewSzoveg = findViewById(R.id.textViewSzoveg);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+            writePermission = false;
+            String[] permissions =
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE};
+            ActivityCompat.requestPermissions(this, permissions, 1);
+        }else{
+            writePermission = true;
+        }
     }
 }
