fdc0c60c9c04025b02f3c19d59717dcc25aa0821
Szalai Szabolcs <sz.szalai16@gmail.com>
2022-01-29 21:31:39 +0100

    qrcode generálás, kiírás

--- app/src/main/java/hu/petrik/qrdolgozat/MainActivity.java
+++ app/src/main/java/hu/petrik/qrdolgozat/MainActivity.java
@@ -6,18 +6,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -27,9 +37,14 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnScan, btnKiir;
+    private Button btnScan, btnKiir, btnKoord;
     private TextView textViewSzoveg;
+    private ImageView imageViewKoordQr;
     private boolean writePermission;
+    private double longitude;
+    private double latitude;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,10 +67,23 @@ public class MainActivity extends AppCompatActivity {
         btnKiir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED ||
+                        ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                                != PackageManager.PERMISSION_GRANTED) {
+
+                    writePermission = false;
+                    String[] permissions =
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                                    Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                    ActivityCompat.requestPermissions(MainActivity.this, permissions, 0);
+                } else{
+                    writePermission = true;
+                }
                 if (writePermission) {
                     Date date = Calendar.getInstance().getTime();
                     SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                    String s = textViewSzoveg.getText().toString() + ", " + format.format(date);
+                    String s = String.format("%s, %s, %.2f, %.2f", textViewSzoveg.getText().toString(), format.format(date), longitude, latitude);
 
                     try {
                         FileOutputStream fileOutputStream = openFileOutput("scannedCodes.csv", MODE_PRIVATE);
@@ -66,9 +94,58 @@ public class MainActivity extends AppCompatActivity {
                         e.printStackTrace();
                     }
                 }
-
             }
         });
+
+        btnKoord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = String.format("%.2f;%.2f",longitude, latitude);
+                MultiFormatWriter mfw = new MultiFormatWriter();
+                try {
+                    BitMatrix bitMatrix = mfw.encode(text, BarcodeFormat.QR_CODE, 500,500);
+                    BarcodeEncoder bce = new BarcodeEncoder();
+                    Bitmap bitmap = bce.createBitmap(bitMatrix);
+                    imageViewKoordQr.setImageBitmap(bitmap);
+                }catch (WriterException e){
+                    e.printStackTrace();
+                }
+                //Toast.makeText(MainActivity.this, "Longitude:"+Double.toString(longitude)+"\nLatitude:"+Double.toString(latitude), Toast.LENGTH_SHORT).show();
+                textViewSzoveg.setText(String.format("%.2f;%.2f",longitude, latitude));
+            }
+        });
+    }
+
+    public void init() {
+        btnScan = findViewById(R.id.btnScan);
+        btnKiir = findViewById(R.id.btnKiir);
+        btnKoord = findViewById(R.id.btnKoord);
+        imageViewKoordQr = findViewById(R.id.imageViewKoordQr);
+        textViewSzoveg = findViewById(R.id.textViewSzoveg);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        locationListener = location -> {
+            longitude = location.getLongitude();
+            latitude = location.getLatitude();
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+            String[] permissions =
+                    new String[]{
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION
+                    };
+            ActivityCompat.requestPermissions(this, permissions, 1);
+            return;
+        }
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                0, 0, locationListener);
     }
 
     @Override
@@ -94,6 +171,16 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 1) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED ||
+                    ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED)
+            {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                        0, 0, locationListener);
+            }
+        }
         if (requestCode == 0) {
             writePermission =
                     ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
@@ -102,25 +189,5 @@ public class MainActivity extends AppCompatActivity {
                                     == PackageManager.PERMISSION_GRANTED;
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
-
-    public void init() {
-        btnScan = findViewById(R.id.btnScan);
-        btnKiir = findViewById(R.id.btnKiir);
-        textViewSzoveg = findViewById(R.id.textViewSzoveg);
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED ||
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                        != PackageManager.PERMISSION_GRANTED) {
-
-            writePermission = false;
-            String[] permissions =
-                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
-                            Manifest.permission.WRITE_EXTERNAL_STORAGE};
-            ActivityCompat.requestPermissions(this, permissions, 1);
-        }else{
-            writePermission = true;
-        }
     }
 }
